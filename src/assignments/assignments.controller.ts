cfa39fd122c8e954edc6845c8116ae7d413f3869
jcualizares <168882211+jcualizares@users.noreply.github.com>
2024-11-07 02:43:42 +0800

    Completed Assignment 3: Factorial Calculator

--- src/assignments/assignments.controller.ts
+++ src/assignments/assignments.controller.ts
@@ -19,5 +19,14 @@ export class AssignmentsController {
     }
     return { isPrime: true };
   }
-  
+
+  @Get('factorial/:number')
+  calculateFactorial(@Param('number') number: number) {
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+      factorial *= i;
+    }
+    return { factorial };
+  }
+
 }
